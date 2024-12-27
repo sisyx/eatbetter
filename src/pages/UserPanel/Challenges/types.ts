54a6c4d703641bc43d240c31d0876a7ecc469650
sisyx <bahadorisajad908@gmail.com>
2024-12-27 19:48:13 +0330

    made dynamic: user challenges page

---
+++ src/pages/UserPanel/Challenges/types.ts
@@ -0,0 +1,51 @@
+const typesOfChallenges = [
+    {
+        value: "challangeEnetaf",
+        title: "چالش های انعطافی",
+        titleEn: "Flexablity Challenges"
+    },
+    {
+        value: "challangeSineWomen",
+        title: "چالش های زنان",
+        titleEn: "Flexablity Challenges"
+    },
+    {
+        value: "challangeCleanBody",
+        title: "چالش های بدن تمیز",
+        titleEn: "Flexablity Challenges"
+    },
+    {
+        value: "challangeSineMen",
+        title: "چالش های مردان",
+        titleEn: "Flexablity Challenges"
+    },
+    {
+        value: "challangeStongLeg",
+        title: "چالش های پای قوی",
+        titleEn: "Flexablity Challenges"
+    },
+    {
+        value: "challangeFitBasan",
+        title: "چالش های فیتنس باسن",
+        titleEn: "Flexablity Challenges"
+    },
+    {
+        value: "challangeFitArm",
+        title: "چالش های ساعد",
+        titleEn: "Flexablity Challenges"
+    },
+    {
+        value: "challangeLowStress",
+        title: "چالش های کاهش استرس",
+        titleEn: "Flexablity Challenges"
+    },
+]
+
+type TypeOfChallengeType = {
+    value: string,
+    title: string,
+    titleEn: string,
+}
+
+export { typesOfChallenges };
+export type { TypeOfChallengeType }
