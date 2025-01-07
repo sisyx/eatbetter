839554106a9188f33d1f22a409b30f49d4b63321
KasraMg <kasragithub7@gmail.com>
2025-01-07 13:04:42 +0330

    fix scrollToUp

--- src/utils/ScrollToUp.tsx
+++ src/utils/ScrollToUp.tsx
@@ -1,9 +1,19 @@
 import { useState, useEffect } from "react";
 import { FaArrowUp } from "react-icons/fa";
 import { Button } from "../components/shadcn/ui/button";
+import { useLocation } from "react-router-dom";
 
 const ScrollToUp = () => {
   const [isVisible, setIsVisible] = useState(false);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    document.documentElement.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "instant",
+    });
+  }, [pathname]);
 
   useEffect(() => {
     const handleScroll = () => {
